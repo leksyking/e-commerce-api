f5075dfe96aacf5e2a8f1aa2d1995ecb63c4b723
Leksyking <gbemilekeogundipe@gmail.com>
2022-02-13 08:16:31 -0800

    add cookies"

--- utils/jwt.js
+++ utils/jwt.js
@@ -9,7 +9,17 @@ const createJWT = ({payload}) => {
 const isTokenValid = ({token}) => jwt.verify(token, process.env.JWT_SECRET)
 
 const attachCookiesToResponse = ({res, user}) => {
-    return createJWT({payload: user})
+    const token = createJWT({payload: user});
+
+    
+  const oneDay = 1000 * 60 * 60 * 24;
+  
+    res.cookie('token', token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + oneDay),
+        secure: process.env.NODE_ENV === 'production',
+        signed: true,
+      });
 }
 
 
