2e2a72a9d50fc9c0b4c1d2111567ab2cf6831cc0
Leksyking <gbemilekeogundipe@gmail.com>
2022-02-13 11:07:20 -0800

    add cookie-parser

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 require('express-async-errors')
 
 const express = require('express')
+const cookieParser = require('cookie-parser')
 
 //imported modules
 const connectDB = require('./db/connect')
@@ -14,6 +15,7 @@ const app = express()
 
 //middlewares
 app.use(express.json())
+app.use(cookieParser(process.env.COOKIE_P))
 
 //Routes
 app.get('/', (req, res) => {
