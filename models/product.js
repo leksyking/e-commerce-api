5b6d0fc55bcbf84a200a7f757871a1e92e38b3e3
Leksyking <gbemilekeogundipe@gmail.com>
2022-02-18 14:24:43 -0800

    create product schema

--- models/product.js
+++ models/product.js
@@ -1,5 +1,44 @@
 const mongoose = require('mongoose')
 
 const ProductSchema = new mongoose.Schema({
-    
-})
+    name : {
+        type: String
+    },
+    price: {
+        type: Number
+    },
+    desciption:{
+        type: String
+    },
+    image: {
+        type:  String
+    },
+    category: {
+        type: String
+    },
+    company: {
+        type: String
+    },
+    colors: {
+        type: []
+    },
+    featured: {
+        type: Boolean
+    },
+    freeShipping: {
+        type: Boolean
+    },
+    inventory: {
+        type: Number
+    },
+    averageRating: {
+        type: Number
+    },
+
+
+
+},{timestamps: true})
+
+const Product = mongoose.model('Product', ProductSchema)
+
+module.exports =Product
