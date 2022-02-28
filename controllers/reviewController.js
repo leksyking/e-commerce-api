0fd1df1014b48d8edaaa16eff8b3fbc40753e5eb
Leksyking <gbemilekeogundipe@gmail.com>
2022-02-28 15:30:08 -0800

    implement review controller

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,23 +1,51 @@
 const Review = require('../models/review')
-
+const {BadRequestError, notFoundError} = require('../errors')
+const {StatusCodes} = require('http-status-codes')
+const {} = require('../utils')
 
 const createReview = async (req, res) => {
     req.body.user = req.user.userId
-    //get product id
-    const review = await Review.create()
-    res.send('Create a review')
+    if(!req.body.product){
+        throw new BadRequestError('Please provide the product')
+    }
+    const review = await Review.create(req.body)
+    res.status(StatusCodes.OK).json({review})
 }
 const getAllReviews = async (req, res) => {
-    res.send('Get all reviews')
+    const review = await Review.find({})
+    res.status(StatusCodes.OK).json({review})
 }
 const getSingleReview = async (req, res) => {
-    res.send('Get a single review')
+    const {id: reviewId} = req.params
+    const review = await Review.findOne({_id: reviewId})
+    if(!review){
+        throw new notFoundError(`User with id: ${reviewId} does not exist.`)
+    }
+    res.status(StatusCodes.OK).json({review})
 }
 const updateReview = async (req, res) => {
-    res.send('Update review')
+    const {rating, title, comment} = req.body
+    const {id: reviewId} = req.params
+    const review = await Review.findOne({_id: reviewId})
+    if(!review){
+        throw new notFoundError(`User with id: ${reviewId} does not exist.`)
+    }
+    checkPermissions(req.user, review.user)
+    review.rating = rating;
+    review.title = title;
+    review.comment = comment;
+    await review.save();
+    res.status(StatusCodes.OK).json({review})
 }
 const deleteReview = async (req, res) => {
-    res.send('Delete a review')
+    const {id: reviewId} = req.params
+    const  review = await Review.find({_id: reviewId})
+    if(!review){
+        throw new notFoundError(`User with id: ${reviewId} does not exist.`)
+    }
+    checkPermissions(req.user, review.user)
+    await review.remove()
+    res.status(StatusCodes.OK).json({msg: "Success"})
 }
 
 module.exports = {
