a819b8cf5c2b0830b64a3f94fb5b11cc929fc9aa
Leksyking <gbemilekeogundipe@gmail.com>
2022-02-28 18:45:40 -0800

    check for product in create review

--- controllers/reviewController.js
+++ controllers/reviewController.js
@@ -1,19 +1,27 @@
 const Review = require('../models/review')
 const {BadRequestError, notFoundError} = require('../errors')
 const {StatusCodes} = require('http-status-codes')
-const {} = require('../utils')
+const {checkPermissions} = require('../utils')
+const Product = require('../models/product')
 
 const createReview = async (req, res) => {
-    req.body.user = req.user.userId
-    if(!req.body.product){
-        throw new BadRequestError('Please provide the product')
+    const {product: productId} = req.body
+    console.log(productId)
+    const validProduct = await Product.findOne({_id : productId})
+    if(!validProduct){
+        throw new notFoundError(`User with id: ${productId} does not exist.`)
     }
+    const validReview = await Review.findOne({product: productId, user: req.user.userId})
+    if(validReview){
+        throw new BadRequestError('You created a review already')
+    }
+    req.body.user = req.user.userId
     const review = await Review.create(req.body)
     res.status(StatusCodes.OK).json({review})
 }
 const getAllReviews = async (req, res) => {
-    const review = await Review.find({})
-    res.status(StatusCodes.OK).json({review})
+    const reviews = await Review.find({})
+    res.status(StatusCodes.OK).json({reviews})
 }
 const getSingleReview = async (req, res) => {
     const {id: reviewId} = req.params
@@ -39,7 +47,7 @@ const updateReview = async (req, res) => {
 }
 const deleteReview = async (req, res) => {
     const {id: reviewId} = req.params
-    const  review = await Review.find({_id: reviewId})
+    const  review = await Review.findOne({_id: reviewId})
     if(!review){
         throw new notFoundError(`User with id: ${reviewId} does not exist.`)
     }
