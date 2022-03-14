a1e179af340ffaf0adc4f166300643ca845b3dce
Leksyking <gbemilekeogundipe@gmail.com>
2022-03-13 22:07:14 -0700

    add security packages

--- app.js
+++ app.js
@@ -5,6 +5,12 @@ const express = require('express')
 const cookieParser = require('cookie-parser')
 const fileUpload = require('express-fileupload')
 
+//security packages
+const cors = require('cors')
+const helmet = require('helmet')
+const rateLimit = require('express-rate-limit')
+const xss = require('xss-clean')
+
 //imported modules
 const connectDB = require('./db/connect')
 const errorHandlerMiddleware = require('./middlewares/errorHandler')
@@ -16,6 +22,21 @@ const reviewRouter = require('./routes/reviewRoute')
 const orderRouter = require('./routes/orderRoute')
 
 const app = express()
+
+app.set('trust proxy', 1)
+//use security packages
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+app.use(limiter)
+app.use(cors())
+app.use(xss())
+app.use(helmet())
+
+
 
 //middlewares
 app.use(express.json())
