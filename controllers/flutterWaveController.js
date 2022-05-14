08d407b981a71c22cd037b4de7d64d96d445236f
Leksyking <gbemilekeogundipe@gmail.com>
2022-05-14 05:09:31 -0700

    handle payment response

---
+++ controllers/flutterWaveController.js
@@ -0,0 +1,66 @@
+const Wallet = require('../models/wallet')
+const Transaction = require('../models/transaction')
+const WalletTransaction = require('../models/walletTransaction')
+const { BadRequestError } = require('../errors')
+
+const response = async(req, res) =>{
+    const {transaction_id} = req.query
+    const url = `https://api.flutterwave.com/v3/transactions/${transaction_id}/verify`;
+
+    const response = await axios({
+        url,
+        method: "get",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+            Authorization: `${process.env.SECRET_KEY}`,
+        }
+    })
+    console.log(response.data.data)
+    const {status, currency, customer, id, amount}  = response.data.data;
+
+    //check if user has a wallet
+    const wallet = await validateUserWallet()
+    await createWalletTransaction({status, currency, amount})
+    //check if transaction id exists already
+    const transaction = await Transaction.findOne({transactionId: id})
+    if (transaction){
+        throw new BadRequestError("Transaction id already exists.")
+    }
+    await createTransaction({id, status, currency, amount, customer})
+    await updateWallet(amount)
+
+    res.status(200).json({
+        response: "Wallet funded successfuly",
+        data: wallet
+    })
+
+}
+
+const validateUserWallet = async ()=>{
+    const userWallet = await Wallet.findOne({userId: req.user.userId})
+    if (!userWallet){
+        const wallet = await Wallet.create({userId: req.user.userId})
+        return wallet
+    }
+    return userWallet
+}
+
+const createWalletTransaction = async ({status, currency, amount})=>{
+    const walletTransaction = await WalletTransaction.create({amount, isInflow: true, currency, status, userId: req.user.userId})
+    return walletTransaction
+}
+
+const createTransaction = async ({id, status, currency, amount, customer})=>{
+    const transaction = await Transaction.create({userId: req.user.userId, 
+        paymentStatus: status, paymentGateway: "flutterwave", amount, currency, 
+        transactionId: id, name: customer.name, email: customer.email, phone: customer.phone_number
+    })
+    return transaction
+}
+
+const updateWallet = async (amount) =>{
+    //remember do do {$inc}
+    const wallet = await Wallet.findOneAndUpdate({userId: req.user.userId}, {balance: amount}, {new: true})
+    return wallet
+}
